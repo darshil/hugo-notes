a57af0cf1d6fdfd042970cd5c3989bcdd5de100a
Darshil Patel <darshil-patel@hotmail.com>
2017-02-18 19:17:40 +0000

    things are going slow; add all posts controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,13 +1,15 @@
 class DocsController < ApplicationController
     
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+    
     def index
     end
     
-    def show    
+    def show
     end  
     
     def new 
-        @doc = Doc.new
+        @doc = Doc.new 
     end
     
     def create
@@ -31,6 +33,7 @@ class DocsController < ApplicationController
     
     private
        def find_doc
+           @doc = Doc.find(params[:id])
        end
        
        def doc_params
