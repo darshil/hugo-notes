dfcebc466edbea1acab2795f11e26078db58eb1a
Darshil Patel <darshil-patel@hotmail.com>
2017-02-18 21:32:54 +0000

    change root if(auth)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+  
   root 'welcome#index'
   
   resources :docs
