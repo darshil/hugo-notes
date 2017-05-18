027352150eec762329a1a4d119a8d10010ade473
Darshil Patel <darshil-patel@hotmail.com>
2017-05-18 05:45:23 +0000

    fixed heroku hosting error

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 20170218211453) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
